05a2cca5cca1cc2cbddea591380e32ff8b38a3b6
Nicolas <nicopazmalizia@gmail.com>
2024-06-14 02:18:25 -0300

    Modifico cors con direccion correcta

--- src/app.js
+++ src/app.js
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(
   cors({
-    origin: "https://encuestas-front-t25n.onrender.com",
+    origin: "https://encuestapp-backend.onrender.com",
     credentials: true,
   })
 );
